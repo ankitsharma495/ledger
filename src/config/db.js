d529104ec923f2d007690fd3d3194cc99bcfc382
Ankit Sharma <ankit.cse2021@ritroorkee.com>
2026-03-28 16:19:19 +0530

    feat: restructure project and implement core banking features
    - Removed outdated package history file.
    - Updated package.json and package-lock.json to include new dependencies: bcryptjs, cookie-parser, jsonwebtoken, and nodemailer.
    - Created server.js as the entry point for the application.
    - Developed app.js to set up Express server and middleware.
    - Implemented database connection logic in db.js.
    - Added account management controllers and routes for creating and retrieving accounts.
    - Developed authentication controllers and routes for user registration, login, and logout.
    - Created transaction management controllers and routes for processing transactions.
    - Implemented middleware for authentication and authorization.
    - Defined Mongoose models for user, account, transaction, ledger, and token blacklist.
    - Integrated email service for sending notifications upon registration and transaction completion.

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+function connectToDB(){
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("db connected");
+    })
+}
+
+module.exports= connectToDB;
